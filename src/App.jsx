0febd8f741a99e80c0a888a1193a3ed581ade053
omk963 <132976939+omk963@users.noreply.github.com>
2024-12-08 23:18:49 -0800

    feat(recipeCards): initial commit of the recipe cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { getAllData } from './util/index';
+import RecipeList from './components/RecipeList';
+import { recipes } from './list';
+
 
 const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
-  
-  const [message, setMessage] = useState(''); 
 
-  useEffect(() => {
+  // const [message, setMessage] = useState('');
 
-    (async () => {
-      const myData = await getAllData(URL)
-      setMessage(myData.data);
-    })();
-      
-    return () => {
-      console.log('unmounting');
-    }
+  // useEffect(() => {
 
-  }, []);
+  //   (async () => {
+  //     const myData = await getAllData(URL)
+  //     setMessage(myData.data);
+  //   })();
+
+  //   return () => {
+  //     console.log('unmounting');
+  //   }
+
+  // }, []);
+  // <h1>{message}</h1>
 
   return (
     <>
-      <h1>{message}</h1>
+      <RecipeList recipes={recipes} />
     </>
   );
 
