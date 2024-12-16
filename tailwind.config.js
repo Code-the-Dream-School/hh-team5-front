724426a44f18ed91cb3a7c55c806e54ea41b0558
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2024-12-15 23:22:29 -1000

    Set up global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        grey: 'hsl(0, 0%, 65%)',
+        black: 'hsl(0, 0%, 0%)',
+        green: 'hsl(80, 60%, 35%)',
+        yellow: 'hsl(46, 74%, 92%)',
+      },
+      fontFamily: {
+        heading: ['"Fjalla One"', 'sans-serif'],
+        body: ['"Fira Mono"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
