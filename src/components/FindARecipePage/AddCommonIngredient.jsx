11bae3d6343eb222bc540afd7ff912a342916c6d
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2024-12-30 00:11:51 -1000

    Move AddCommonIngredient logic into its own component

---
+++ src/components/FindARecipePage/AddCommonIngredient.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+
+const addButton = <FontAwesomeIcon icon={faCirclePlus} />
+
+const AddCommonIngredient = ({ ingredient, handleAddIngredient }) => {
+  return (
+    <li key={ingredient.id} class="flex flex-row max-w-52 rounded-[30px] min-w-[102.67px] justify-between bg-green text-white px-2 m-2">
+      <span class="px-2 pb-1">{ingredient.title}</span>
+      <button onClick={() => handleAddIngredient(ingredient.id)}>{addButton}</button>
+    </li>
+  );
+};
+
+export default AddCommonIngredient
--- src/components/FindARecipePage/CommonIngredients.jsx
+++ src/components/FindARecipePage/CommonIngredients.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCirclePlus} from '@fortawesome/free-solid-svg-icons';
+import AddCommonIngredient from "./AddCommonIngredient";
 
-const addButton = <FontAwesomeIcon icon={faCirclePlus} />
 
-const CommonIngredients = ({commonIngredients}) => {
+const CommonIngredients = ({commonIngredients, handleAddIngredient}) => {
   return (
     <div class="flex flex-col items-center">
-      <h2 class="font-heading text-black text-xl w-full text-center">Add a common ingredient:</h2>
+      <h2 class="font-heading text-black text-xl w-full text-center m-2">Add a common ingredient:</h2>
       <ul class="grid grid-cols-5 grid-rows-2 max-w-4xl">
         {commonIngredients.map((ingredient) => (
-          <li key={ingredient.id} class="flex flex-row max-w-52 rounded-[30px] min-w-[102.67px] justify-between bg-green text-white px-2 m-2">
-            <p class="px-2 pb-1">{ingredient.title}</p>
-            <button>{addButton}</button>
-          </li>
+          <AddCommonIngredient 
+            key={ingredient.id}
+            ingredient={ingredient}
+            handleAddIngredient={handleAddIngredient}
+          />
           ))
         }
       </ul>
