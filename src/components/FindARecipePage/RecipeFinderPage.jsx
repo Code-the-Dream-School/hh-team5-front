ee1babbab516b80c33bc6feb014634974025b395
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2024-12-30 14:27:20 -1000

    Create functions to add and remove ingredients

--- src/components/FindARecipePage/RecipeFinderPage.jsx
+++ src/components/FindARecipePage/RecipeFinderPage.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CommonIngredients from './CommonIngredients.jsx'
+import SelectedIngredientsList from './SelectedIngredientsList.jsx';
 import { commonIngredients } from "../../commoningredients";
 
 const RecipeFinderPage = () => {
+  const [selectedIngredients, setSelectedIngredients] = useState([])
+
+  const handleAddIngredient = (id) => {
+    const selectedIngredientsList = commonIngredients.find((item) => item.id === id);
+
+    if (selectedIngredients.filter((item) => item.id === id)) {
+      setSelectedIngredients([...selectedIngredients, selectedIngredientsList])
+    }
+  }
+
+  const handleRemoveIngredient = (id) => {
+    setSelectedIngredients(
+      selectedIngredients.filter((ingredient) => ingredient.id !== id)
+    )
+  }
+
   return (
     <div class="bg-yellow min-h-screen flex items-center flex-col p-4">
-      <CommonIngredients commonIngredients={commonIngredients}/>
+      <CommonIngredients commonIngredients={commonIngredients} handleAddIngredient={handleAddIngredient} />
+      <SelectedIngredientsList selectedIngredients={selectedIngredients} handleRemoveIngredient={handleRemoveIngredient} />
     </div>
-  );
-};
+  )
+}
 
-export default RecipeFinderPage;
+export default RecipeFinderPage
