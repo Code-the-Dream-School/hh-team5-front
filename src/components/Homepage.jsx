cebd352e9baa507f69b120c1da9db10f1339bd3b
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2025-01-05 01:27:31 -1000

    Update image and description for route cards

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import foodcollage from '/images/foodcollage.png'
+import ingredients from '/images/ingredients.jpg'
 import meatballs from '/images/meatballs.png'
 
 const Homepage = () => {
@@ -8,7 +8,7 @@ const Homepage = () => {
     {
       id: 1,
       title: "Find a Recipe",
-      image: foodcollage,
+      image: ingredients,
       description: `Find a recipe by entering 3-5 ingredients from your pantry and discover delicious dishes you can make. It’s a simple way to turn what you have on hand into mealtime inspiration!`, 
       link: "/findarecipe",
     },
@@ -16,7 +16,7 @@ const Homepage = () => {
       id: 2,
       title: "Check your Favorites",
       image: meatballs,
-      description: `Browse through your favorite recipes to revisit dishes or spark inspiration for your next meal. Whether you're planning a quick dinner or a special feast, your favorites are just a click away!`, 
+      description: `Browse through your favorite recipes to revisit dishes for your next meal. Whether you're planning a quick dinner or a special feast, your favorites are just a click away!`, 
       link: "/favorites"
     }
   ]
