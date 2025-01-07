c026164ff0519708e74ab178da7c37f430612057
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2025-01-07 01:18:31 -1000

    Add styles to navbar

--- src/components/Header/NavLink.jsx
+++ src/components/Header/NavLink.jsx
@@ -1,16 +1,14 @@
-// This component is responsible for the navigation links.
 import React from 'react';
-import { Link } from 'react-router-dom'; // To use NavLink
+import { Link } from 'react-router-dom'; 
 
 const NavLink = () => {
   return (
-    <nav>
-      <Link to="/">  Home</Link>
-      <Link to="/favorites">  Favorites</Link>
-      {/* login/logout based on status */}
-      <Link to="/logout">  Logout</Link>
+    <nav class="flex justify-end bg-[#415616] font-heading text-white text-xl pr-8">
+      <Link class="m-3 mx-8" to="/">Home</Link>
+      <Link class="m-3 mx-8" to="/favorites">Favorites</Link>
+      <Link class="m-3 mx-8" to="/logout">Logout</Link>
     </nav>
-  );
-};
+  )
+}
 
-export default NavLink;
+export default NavLink
