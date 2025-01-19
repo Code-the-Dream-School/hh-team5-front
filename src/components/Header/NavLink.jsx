2f92ff83e95ecc504333043ae00955a6fc963eb9
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2025-01-18 20:51:02 -1000

    Add styles for mobile design

--- src/components/Header/NavLink.jsx
+++ src/components/Header/NavLink.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom'; 
 
 const NavLink = () => {
+  
   return (
-    <nav className="flex justify-end bg-[#415616] font-heading text-white text-xl pr-8">
-      <Link className="m-3 mx-8" to="/">Home</Link>
-      <Link className="m-3 mx-8" to="/favorites">Favorites</Link>
-      <Link className="m-3 mx-8" to="/logout">Logout</Link>
+    <nav className="flex justify-center bg-[#415616] font-heading text-white text-l min-[420px]:text-xl md:justify-end md:text-2xl md:pr-8">
+      <Link className="my-4 mx-4 hover:underline sm:mx-6 md:mx-8" to="/">Home</Link>
+      <Link className="my-4 mx-4 hover:underline sm:mx-6 md:mx-8" to="/findarecipe">Find a Recipe</Link>
+      <Link className="my-4 mx-4 hover:underline sm:mx-6 md:mx-8" to="/favorites">Favorites</Link>
+      <Link className="my-4 mx-4 hover:underline sm:mx-6 md:mx-8" to="/">Logout</Link>
     </nav>
   )
 }
