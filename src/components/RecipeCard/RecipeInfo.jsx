333a2d8f27cf17476668d207de5b1e4f8ed1a718
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2025-01-21 06:17:37 -1000

    Add truncate property to h3 to handle overflow

--- src/components/RecipeCard/RecipeInfo.jsx
+++ src/components/RecipeCard/RecipeInfo.jsx
@@ -2,7 +2,7 @@
 const RecipeInfo = ({ name, time }) => {
     return (
         <div className="relative bg-white text-left p-2 px-4 mt-[-2rem] rounded-xl z-30 drop-shadow-[0_2px_2px_rgba(0,0,0,0.5)]">
-            <h3 className="font-heading text-l">{name}</h3>
+            <h3 className="font-heading text-l truncate w-[16rem]">{name}</h3>
             <p className="font-body">{time}</p>
         </div>
     );
