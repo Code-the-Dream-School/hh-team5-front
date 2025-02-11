cc5154c2ecb23501ca24c4212dcc38765a657fa2
Jin <tcofte7827@gmail.com>
2025-02-10 22:27:02 -0800

    changed backend API URL

--- src/util/index.js
+++ src/util/index.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1', // Backend API URL
+  // baseURL: 'http://localhost:8000/api/v1', // Backend API URL
+  baseURL: "https://hh-team5-back.onrender.com",
   withCredentials: true,
   timeout: 10000, // Timeout after 10 seconds
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
