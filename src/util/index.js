7f5825a177df73a802c037fcb923a08a44e0717f
Jin <tcofte7827@gmail.com>
2025-02-11 22:49:04 -0800

    added env variable

--- src/util/index.js
+++ src/util/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: 'http://localhost:8000/api/v1', // Backend API URL
-  baseURL: "https://hh-team5-back.onrender.com",
+  baseURL: import.meta.env.VITE_BACK_URL,
   withCredentials: true,
   timeout: 10000, // Timeout after 10 seconds
   headers: {
